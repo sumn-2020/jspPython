16ad7200797592172efa37f0423e843d338dc51c
sunmin <dltjsals6356@naver.com>
2022-12-15 21:02:50 +0900

    chat 파이썬

--- python_eclipse/HELLO_PYTHON/day17/myflask.py
+++ python_eclipse/HELLO_PYTHON/day17/myflask.py
@@ -1,48 +1,46 @@
-from flask import Flask
-from flask import request
-import psycopg2
-from flask import Flask,render_template
-from flask.json import jsonify
-
-
-from flask import Flask
-from flask_socketio import SocketIO
-
-
-
-
+from flask import Flask, jsonify, render_template
+from subprocess import call
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '1234'
-socketio = SocketIO(app)
+app.secret_key = "mysecret"
+
+socket_io = SocketIO(app)
+
+# websocket flask
+# pip install flask-socketio
+# pip install eventlet
+# pip install --upgrade python-socketio==4.6.0
+# pip install --upgrade python-engineio==3.13.2
+# pip install --upgrade Flask-SocketIO==4.3.1
 
 
+
+@app.route('/chat2')
+def chat2():
+    return render_template('chat2.html')
 
 
 @app.route('/')
-@app.route('/three')
-def sessions():
-    return render_template('session.html')
-
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
-    
-    
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)   
+@app.route('/allready')
+def allready():
+    return render_template('allready.html')
 
 
+
+@socket_io.on("message")
+def request(message):
+    print("message : "+ message) 
+    mydict = dict()
+    mydict['message'] = message  # 자바스크립트 mydict.message
+    send(mydict, broadcast=True) #broadcast => 현재 접속한 사람들에게 송출  #broadcast vs echo
+   
+   
 
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    socketio.run(app, debug=True)
-    
-    
-    
-    
+    socket_io.run(app, host="0.0.0.0", debug=True, port=9999)
+    #host="0.0.0.0" => 0.0.0.0 모든 ip들을 전부 허용하겠다 (이걸 설정 안하면 127.0.0.1만 허용되고 나머지 ip는 허용 안됨)
     
     
     
